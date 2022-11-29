a434367a9bde3e5f5449e99146dcb97287b5e759
Cody T. Zeitler <codyzeitler12@gmail.com>
2022-11-29 15:56:13 -0700

    feat: initial commit

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Text, Button } from 'react-native';
+import { NumberInput } from './src/NumberInput';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+      <TouchableWithoutFeedback style={{ flex: 1 }} onPress={() => {
+        Keyboard.dismiss();
+      }}>
+        <View style={{
+          flex: 1
+        }}>
+          <View style={{
+            flex: .75
+          }}></View>
+          <View style={{
+            flex: .5,
+          }}>
+            <Text style={{
+              textAlign: 'center',
+              fontSize: '20',
+              fontWeight: 'bold'
+            }}>Vasoactive-inotropic Calculator</Text>
+          </View>
+          <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'stretch'
+          }}>
+            <NumberInput title={'Dopamine (ug/kg/min)'} onChange={(event) => {
+              console.log(event);
+            }}></NumberInput>
+          </View>
+          <View style={{
+            flex: 1,
+          }}>
+            <NumberInput title={'Dobutamine (ug/kg/min)'}></NumberInput>
+          </View>
+          <View style={{
+            flex: 1,
+          }}>
+            <NumberInput title={'Epinephrine (ug/kg/min)'}></NumberInput>
+          </View>
+          <View style={{
+            flex: 1,
+          }}>
+            <NumberInput title={'Norepinephrine (ug/kg/min)'}></NumberInput>
+          </View>
+          <View style={{
+            flex: 1,
+          }}>
+            <NumberInput title={'Milrinone (ug/kg/min)'}></NumberInput>
+          </View>
+          <View style={{
+            flex: 1,
+          }}>
+            <NumberInput title={'Vasopression (mU/kg/min)'}></NumberInput>
+          </View>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent : 'center',
+            alignItems : 'center'
+          }}>
+            <Text>123</Text>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "100%",
+    flexDirection: 'column'
   },
 });
---
+++ src/NumberInput.js
@@ -0,0 +1,57 @@
+import React, { Text, TextInput, View } from 'react-native';
+import {useState} from 'react';
+
+export function NumberInput(props) {
+    const [number, setNumber] = useState(null);
+    return (<View style={{
+        flex: 1
+    }}>
+        <View style={{
+            flex: 1,
+            backgroundColor: 'white',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Text adjustsFontSizeToFit>{props.title}</Text>
+        </View>
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignContent: 'center',
+            flexDirection: 'row'
+        }}>
+            <View style={{
+                flex: 1
+            }}></View>
+            <View style={{
+                flex: 1,
+            }}>
+                <TextInput style={{
+                    backgroundColor: '#d1d1d1',
+                    width: "100%",
+                    height: "100%",
+                    borderRadius : 10
+                }}
+                textAlign ='center'
+                keyboardType='numeric'
+                clearTextOnFocus
+                value={number}
+                onChangeText={(valueAsString) => {
+                    const valueAsNumber = parseFloat(valueAsString);
+                    setNumber(valueAsNumber);
+                    if (props.onChange) {
+                        props.onChange({
+                            value : valueAsNumber,
+                            name : props.title
+                        });
+                    }
+                }}
+                ></TextInput>
+            </View>
+            <View style={{
+                flex: 1
+            }}></View>
+        </View>
+
+    </View>);
+}
